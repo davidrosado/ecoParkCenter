55485190002627d6922117c23be13bcaa248b401
David Rosado <drosado1901@gmail.com>
2022-04-22 08:20:19 -0500

    Guardando primer cambio

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EcoPark Center</title>
+    <meta name="author" content="EcoPark Center">
+    <meta name="description" content="Descripcion de EcoPark"> 
+    <meta name="keywords" content="key 1, key 2, key 3" >
+                    
+    <link rel="canonical" href="http://canonical.com/">
+
+    <meta property="og:site_name" content="EcoPark Center" />
+    <meta property="og:locale" content="es_ES">
+    <meta property="og:title" content="EcoPark Center">
+    <meta property="og:description" content="Descripcion de EcoPark">     
+    <meta property="og:url" content="https://ecoparkcenter.com/" />
+    <meta property="og:type" content="website" /> 
+    <meta property="og:image:secure_url" content="https://ecoparkcenter.com/compartir.jpg" />
+    <meta property="og:image" content="https://ecoparkcenter.com/compartir.jpg" />
+    <meta property="og:image:width" content="600" />
+    <meta property="og:image:height" content="450" />	
+
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+</head>
+<body>
+
+    <?php include 'template_parts/i_header_site.php' ?>
+    <?php include 'template_parts/i_banner_home.php' ?>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<!-- Add the slick-theme.css if you want default styling -->
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+	<!-- Add the slick-theme.css if you want default styling -->
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+	<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>   
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;600;700;900&display=swap" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+    <script src="js/wow.min.js" defer></script>
+    <script type="text/javascript" src="js/custom.js" defer></script>
+    <?php 
+    /*
+        <script type="text/javascript" src="js/script-widgets.js" defer></script>
+        <script type="text/javascript" src="js/script-formularios.js" defer></script>
+    */
+    ?>
+</body>
+</html>
---
+++ template_parts/i_header_site.php
@@ -0,0 +1,9 @@
+<header id="header-site">
+    <div class="container-lg">
+        <div class="container">
+            <div class="row justify-content-between">
+
+            </div>
+        </div>
+    </div>
+</header>
